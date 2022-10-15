2d59e39c97a7258a1f410884f1c6f4e8ee947c3f
RYOTA2528 <1216022@gmail.com>
2022-10-15 23:00:22 +0900

    TalkRoomPageでのトーク雛形の作成・DateTime型を使用するためintlライブラリの導入

--- lib/pages/talk_room_page.dart
+++ lib/pages/talk_room_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase/model/message.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,10 +11,25 @@ class TalkRoomPage extends StatefulWidget {
 }
 
 class _TalkRoomPageState extends State<TalkRoomPage> {
+  List<Message> messageList = [
+    Message(message: "こんにちは", isMe: true, sendTime: DateTime.now()),
+    Message(message: "久しぶりだね！", isMe: false, sendTime: DateTime.now()),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(widget.name)),
+      body: ListView.builder(
+          itemCount: messageList.length,
+          itemBuilder: (context, index) {
+            return Row(
+              children: [
+                Text(messageList[index].message),
+                Text("")
+              ],
+            );
+          }
+      ),
     );
   }
 }
